ab77f190c36d9212056bbbef9fc081a5c1b9f48c
Koros <koroshillary12@gmail.com>
2023-10-21 18:21:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
   id: 'mapbox/satellite-streets-v11', // You can change this to another Mapbox style
-  accessToken: 'sk.eyJ1Ijoia29yb3MiLCJhIjoiY2xuem5kaW81MTY1eTJrb2FzajFpYTlnZCJ9.STN0NtnO8xd78loY8FSjgQ'
+  accessToken: '#'
 }).addTo(map);
 
   var geoJsonLayer;
